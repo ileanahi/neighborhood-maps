ed573e7c8dc60065e871a2d4d787e61990681218
Ileana Argyris <argyris.ileana@gmail.com>
2018-10-02 23:53:05 +0200

    Added Google Maps script

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+ * TO-DO:
+ * Location filter - text input or dropdown that filters map markers and list items
+ * List view of all location names which lists all by default and filters when a filter is applied
+ * Clicking location displays unique information about the location (modal, div, info window) and animates map marker
+ * Map displays all location markers by default
+ * Uses Google Maps API and at least one non-Google Third-party API
+ * All Data requests are retrieved in an asynchronous manner using Fetch API or XMLHttpRequest
+ * Error handling - AJAX error or fail methods (visual indication that API didn't load)
+ * Provide Attribution for source of additional data
+ * Focus is appropriately managed
+ * Elements use semantic elements and ARIA roles are defined
+ * Alternate text describes content of images
+ * Site uses service worker
+ * State control managed appropriately - event handlers passed as props, and state is managed by parent components
+ * At least 5 locations in the model - hard coded or retrieved from a data API
+ */
+
+
 class App extends Component {
   render() {
     return (
