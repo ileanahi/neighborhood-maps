e290a2c15f4def54a66d288f46b3e2e9c407ae3a
Ileana Argyris <argyris.ileana@gmail.com>
2018-10-09 10:33:24 +0200

    Created an input element for the side drawer component

--- src/SideDrawer.js
+++ src/SideDrawer.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const SideDrawer = props => {
+  return (
+    <section>
+      <input aria-label="Filter Venues" placeholder="Filter Venues" value />
+      <ul id="places" />
+    </section>
+  );
+};
+
+export default SideDrawer;
