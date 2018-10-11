e3856a47f661f2c0507fd8b4c50c83d525904146
Ileana Argyris <argyris.ileana@gmail.com>
2018-10-11 19:20:17 +0200

    Made hamburger icon larger

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ class App extends Component {
         <header>
           <nav>
             <h2 id="header-text">Neighborhood Map - Kailua-Kona, Hawaii</h2>
-            <i class="fas fa-bars" />
+            <i class="fas fa-bars fa-lg" />
           </nav>
         </header>
         <main>
