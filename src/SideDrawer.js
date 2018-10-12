0a36ceb1c42f32195cf5b3b031b9cf9def1589ea
Ileana Argyris <argyris.ileana@gmail.com>
2018-10-12 20:17:45 +0200

    Added filter class for input box on sidedrawer component

--- src/SideDrawer.js
+++ src/SideDrawer.js
@@ -4,9 +4,18 @@ import VenueInfo from './VenueInfo';
 const SideDrawer = props => {
   return (
     <section id="side-drawer">
-      <input aria-label="Filter Venues" placeholder="Filter Venues" value />
+      <input
+        aria-label="Filter Venues"
+        placeholder="Filter Venues"
+        className="filter"
+      />
       <ul id="places">
-        <VenueInfo place={props.places} />
+        {/*props.places.venue
+        .map(place => (
+          <li key={place.id} >
+            <h2>${place.name}</h2>
+          </li>
+        )) */}
       </ul>
     </section>
   );
--- src/VenueInfo.js
+++ src/VenueInfo.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const VenueInfo = props => {
-  return (
-    <div className="venue-info">
-      `<h2>${props.place.venue.name}</h2>`
-    </div>
-  );
+  return <div className="venue-info" />;
 };
 
 export default VenueInfo;
